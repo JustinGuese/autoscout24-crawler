b39616c4104994d6330a6579348d1ee306227b4a
Justin Guese <guese.justin@gmail.com>
2020-10-20 17:05:29 +0200

    should contain new gitignore

--- cronMergeJob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-~/miniconda3/envs/autoscout/bin/python ~/autoscout24-crawler/mergeAllDFs.py
-echo date >> ~/autoscout24-crawler/cronlog.txt
--- explorer.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("/home/justin/Documents/python/autoscoutcrawler/data/autos/2020-10-19_18_01_52_502709.csv",delimiter=";")
-
-print(data.describe())
-print(data.head())
