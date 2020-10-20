32c9e1a9ac633da1db00dd428fb798382eade199
Justin Guese <guese.justin@gmail.com>
2020-10-20 17:35:21 +0200

    install script udpates

--- ec2CloudSetup.sh
+++ ec2CloudSetup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh ~/
-chmod ~/Miniconda3*.sh
+chmod +x ~/Miniconda3*.sh
 ~/Miniconda3*.sh
 
 sudo yum install git 
 git clone https://github.com/JustinGuese/autoscout24-crawler ~/autoscout24-crawler
 cd ~/autoscout24-crawler
-conda env create -f environment.yml
+conda create --name autoscout 
 source activate autoscout
-
+conda install pip
+pip install -r requirements.txt
