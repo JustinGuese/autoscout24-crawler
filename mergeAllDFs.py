e1f3251bb36de1d06128bd7d0aef478a00a1ca1e
Justin Guese <guese.justin@gmail.com>
2020-10-20 16:33:17 +0200

    added merge commands for update, delete s3 upload

--- mergeAllDFs.py
+++ mergeAllDFs.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import os
 import numpy as np
+import glob
+
+S3UPLOAD = True
+
 
 folder=r'./data/autos/'
 dfs = []
@@ -25,4 +29,41 @@ print("size before cleanup: ",result.shape)
 result = result.drop_duplicates()
 result = result.dropna(thresh=20)
 print("size after cleanup: ",result.shape)
-result.to_csv(folder+"largeDF.csv")
+
+# load old
+old = pd.read_csv(folder+"largeDF.csv.gz",compression="gzip" )
+new = pd.concat([result,old])
+new = new.drop_duplicates()
+new = new.dropna(thresh=20)
+
+
+new.to_csv(folder+"largeDF.csv.gz",compression="gzip")
+print("New size: ",new.shape)
+# remove old
+fileList = glob.glob(folder+'*.csv')
+for filePath in fileList:
+    try:
+        os.remove(filePath)
+    except:
+        print("Error while deleting file : ", filePath)
+
+if S3UPLOAD:
+    import boto3
+    from botocore.exceptions import NoCredentialsError
+    def upload_to_aws(local_file, bucket, s3_file):
+        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                        aws_secret_access_key=SECRET_KEY)
+
+        try:
+            s3.upload_file(local_file, bucket, s3_file)
+            print("Upload Successful")
+            return True
+        except FileNotFoundError:
+            print("The file was not found")
+            return False
+        except NoCredentialsError:
+            print("Credentials not available")
+            return False
+
+
+    uploaded = upload_to_aws(folder+'largeDF.csv.gz', 'datafortress-frankfurt', 'largeDF.csv.gz')
