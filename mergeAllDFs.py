e81142c290a333d255cefb4af9ae6f1ff752bc9a
Justin Guese <guese.justin@gmail.com>
2020-10-20 16:34:42 +0200

    rem access key

--- mergeAllDFs.py
+++ mergeAllDFs.py
@@ -51,8 +51,7 @@ if S3UPLOAD:
     import boto3
     from botocore.exceptions import NoCredentialsError
     def upload_to_aws(local_file, bucket, s3_file):
-        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
+        s3 = boto3.client('s3')
 
         try:
             s3.upload_file(local_file, bucket, s3_file)
